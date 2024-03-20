ae55a2776bc1c31937abbe7c11bdda8a1bdd2aef
Council <acouncil@deloitte.com>
2024-03-20 13:03:31 -0700

    Updated the musician model

--- models/Musician.js
+++ models/Musician.js
@@ -1,7 +1,12 @@
-const {Sequelize, sequelize} = require('../db');
+const {DataTypes, sequelize} = require('../db');
 
 // TODO - define the Musician model
 let Musician;
+
+Musician = seuelize.define('Musician', {
+    name:DataTypes.STRING,
+    instruement:DataTypes.STRING
+})
 
 module.exports = {
     Musician
